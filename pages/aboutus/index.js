bb7d7d3d9d04868e6fab03b0afc399caea71742b
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-12-22 08:55:35 +0000

    Link component [3b83959bb04ed8c839be3e410f491788660c7a9a]

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,6 +1,26 @@
 // domain.com/aboutus
+
+import Link from "next/link";
+
+const developers = [
+  { id: "1", name: "Yash", role: "Senior Developer" },
+  { id: "2", name: "Vaibhav", role: "Backend Developer" },
+  { id: "3", name: "Suresh", role: "Frontend Developer" },
+];
+
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <ul>
+        {developers.map((dev) => (
+          <li key={dev.id}>
+            <Link href={`./aboutus/${dev.id}`}>{dev.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default AboutPage;
